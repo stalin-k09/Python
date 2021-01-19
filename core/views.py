e2133d442103868f44d9a2176dccb1e8416b70bd
lchopunk <lchopunk@outlook.es>
2021-01-19 15:06:00 -0500

    ramaolmus - agregue comentario de prueba

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-
+#Hola
 def medicamentos(request):
     return HttpResponse(html_cabecera + "<h1>Medicamentos FARMAPRO</h1>")
