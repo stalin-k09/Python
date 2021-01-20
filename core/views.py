5c1ec884e3a7dd242127a7a5d99323eddd6853e2
jcepeda <jorgecepeda22@gmail.com>
2021-01-19 19:46:47 -0500

    Distribuidores

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+html_cabecera = """
+    <h1>Welcome</h1>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">Distribuidores</a></li>
+    </ul>
+"""
+
+#Create your views here.add()
+def home (request):
+    return HttpResponse(html_cabecera + "<h1> HOME PAGE</h1>")
+
+def distribuidores (request):
+    return HttpResponse(html_cabecera + "<h1> MODULO DISTRIBUIDORES</h1>")
