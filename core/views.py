927b08dac2811d3dd85eb732e7944faa4885922a
lcholmus <lchopunk@outlook.es>
2021-01-20 00:21:17 -0500

    Revert "Revert "Revert "Revert "Ingreso de Datos - Luis Olmus""""
    This reverts commit 7c24a55cc8a3db87c66e011fe9a4ddcda2ea0d46.

--- core/views.py
+++ core/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 #Hola
 def medicamentos(request):
-    return HttpResponse(html_cabecera + "<h1>Medicamentos FARMAPRO - QuitSoft </h1>")
+    return HttpResponse(html_cabecera + "<h1>Medicamentos FARMAPRO</h1>")
+
+ <li><a href="/presentacion">Presntacion</a></li>
