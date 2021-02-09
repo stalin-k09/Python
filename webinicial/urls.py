d662944c5c9731f8608f06ed93dce06446c9716e
GeoEleiker <jorgecepeda22@gmail.com>
2021-02-09 12:20:42 -0500

    crud distribuidor
    distribuidor_confirm_delete
    distribuidor_form
    distribuidor_list
    distribuidor_menu
    distribuidor_update_form

--- webinicial/urls.py
+++ webinicial/urls.py
@@ -13,12 +13,13 @@ from compras import views as compras_views
 from . import settings 
 from medicamentos.urls import pathMedicinas
 from laboratorios.urls import pathLaboratorios
+from distribuidores.urls import pathDistribuidores
 
 urlpatterns = [
     path('', core_views.home, name="home"), #home/ ---> como no tiene nada se redicrecciona al home 
     path('usuarios/', core_views.usuarios, name="usuarios"),
     #path('clientes/', core_views.clientes, name="clientes"),
-    path('distribuidores/', distribuidores_views.distribuidores, name="distribuidores"),
+    #path('distribuidores/', distribuidores_views.distribuidores, name="distribuidores"),
     #path('ventas/', core_views.ventas, name="ventas"),
     path('login/', core_views.login, name="login"),
     path('registro/', core_views.registro, name="registro"),
@@ -26,6 +27,7 @@ urlpatterns = [
     path('clientes/', clientes_views.clientes, name="clientes"),
     path('compras/', compras_views.compras, name="compras"),
 
+    path("distribuidores/", include(pathDistribuidores)),
     path("medicamentos/", include(pathMedicinas)),
     path("laboratorios/", include(pathLaboratorios)),
 ]
